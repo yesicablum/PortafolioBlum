81eebd234eccae15420c2400b2b1acd72474350f
yesicablum <yesica.blum@uniminuto.edu.co>
2025-03-26 16:52:25 -0500

    Cambios en imagenes y agregacion de codigo

--- src/pages/Portafolio.jsx
+++ src/pages/Portafolio.jsx
@@ -14,11 +14,22 @@ const Portafolio = () => {
                 </div>
             </header>
             <main>
-                <section id="about" className="section">
-                    <h2>Sobre Mí</h2>
-                    <p>Hola, soy [Tu Nombre]. Soy desarrollador web apasionado por crear soluciones modernas y funcionales.</p>
+                <div className="boton-scroll">
+                    <a href="#about" >
+                        <img src="magdalena.png" alt="" />
+                    </a>
+                </div>
+                <section id="about" className="section perfil">
+                    <div className="Perfil-content typewriter">
+                        <h1>Sobre Mí</h1>
+                        <p>Hola, soy [Tu Nombre]. Soy desarrollador web apasionado por crear soluciones modernas y funcionales.</p>
+                    </div>
+                    <div className="image-container">
+                        <img src="2.jpg" alt="Mi Foto" />
+                    </div>
+
                 </section>
-                <section id="technologies" className="section">
+                <section id="technologies" className="section skills">
                     <h2>Tecnologías que uso</h2>
                     <div className="technologies">
                         <div className="tech">HTML</div>
@@ -33,24 +44,27 @@ const Portafolio = () => {
                     <div className="projects">
                         <div className="project-card">
                             <h3>GreenWork</h3>
-                            <p>GreenWork es una plataforma web que conecta freelancers locales con oportunidades laborales en el Corredor Ecológico de Villavicencio (2,900 ha), entre el campus San Juan Eudes y la granja Agroecológica UNIMINUTO. Su objetivo es fomentar el empleo y reducir la informalidad en la prestación de servicios.</p>
                             <a href="https://github.com/StarFront/microproyectos_corredor" target="_blank" rel="noopener noreferrer">
-                                <img src="logo-github.png" alt="GitHub" />
+                                <img src="corredor.png" alt="GitHub" style={{ width: '300px', height: 'auto' }} />
                             </a>
+                            <p>GreenWork es una plataforma web que conecta freelancers locales con oportunidades laborales en el Corredor Ecológico de Villavicencio (2,900 ha), entre el campus San Juan Eudes y la granja Agroecológica UNIMINUTO. Su objetivo es fomentar el empleo y reducir la informalidad en la prestación de servicios.</p>
+
                         </div>
                         <div className="project-card">
                             <h3>Gestión de productos</h3>
-                            <p>El aplicativo web permite la gestión de productos mediante un CRUDE, optimizando el control de inventario y ventas en tiempo real. También ofrece la opción de descargar reportes detallados de ventas e inventario para análisis y toma de decisiones.</p>
                             <a href="https://github.com/yesicablum/Frontendcockroachdb" target="_blank" rel="noopener noreferrer">
-                                <img src="https://drive.google.com/file/d/1MRDWYUdmwmy_XJ3P-p030zEsPlLpT83n/view?usp=sharing" alt="GitHub" />
+                                <img src="gestion.jpg" alt="GitHub" style={{ width: '300px', height: 'auto' }} />
                             </a>
+                            <p>El aplicativo web permite la gestión de productos mediante un CRUDE, optimizando el control de inventario y ventas en tiempo real. También ofrece la opción de descargar reportes detallados de ventas e inventario para análisis y toma de decisiones.</p>
+
                         </div>
                         <div className="project-card">
                             <h3>Calculadora Funcional</h3>
-                            <p>El proyecto Calculadora Funcional es una aplicación desarrollada para realizar operaciones matemáticas de manera eficiente e intuitiva. Su implementación permite ejecutar cálculos básicos con una interfaz amigable y funcional.</p>
                             <a href=" https://github.com/yesicablum/CALCULADORA" target="_blank" rel="noopener noreferrer">
-                                <img src="logo-github.png" alt="GitHub" />
+                                <img src="calculadora.png" alt="GitHub" style={{ width: '300px', height: 'auto' }} />
                             </a>
+                            <p>El proyecto Calculadora Funcional es una aplicación desarrollada para realizar operaciones matemáticas de manera eficiente e intuitiva. Su implementación permite ejecutar cálculos básicos con una interfaz amigable y funcional.</p>
+                           
                         </div>
                     </div>
                 </section>
